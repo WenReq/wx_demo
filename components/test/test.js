b8567cf97144304870d08cf3720297d878c4e264
wenreq <294491328@qq.com>
2022-09-19 22:19:58 +0800

    add组件自定义样式和data/methods

--- components/test/test.js
+++ components/test/test.js
@@ -1,5 +1,10 @@
 // components/test/test.js
 Component({
+  options: {
+    // apply-shared 全局样式影响组件样式
+    // shared 组件影响全局，全局也影响组件
+    styleIsolation: "shared"
+  },
   /**
    * 组件的属性列表
    */
@@ -11,13 +16,22 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    count: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    addCount() {
+      this.setData({ count: this.data.count + 1 })
+      this._showCount()
+    },
+    _showCount() { // 自定义方法建议以 _ 开头
+      wx.showToast({
+        title: 'count值为：' + this.data.count,
+        icon: 'none'
+      })
+    }
   }
 })
